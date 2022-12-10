138d05f5cc29357c5b7a18a74d8be2ceb68edfcf
Yajat Vishwak <yajat472001@gmail.com>
2022-12-10 08:45:18 +0530

    asd

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import Application from "./pages/Application.svelte";
+import TakeAnalysis from "./pages/TakeAnalysis.svelte";
+import AllTakes from "./pages/AllTakes.svelte";
 
 const routes = {
   "/": Application,
+  "/take/:id": TakeAnalysis,
+  "/alltakes": AllTakes,
 };
 export default routes;
